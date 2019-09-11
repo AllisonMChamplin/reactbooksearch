f51436a94b4b970769ae9b2228f36b82ddddc105
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-09-11 18:16:56 -0500

    Updated nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -11,9 +11,6 @@ function Nav() {
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-          </li>
           <li className="nav-item">
             <a className="nav-link" href="/search">Search</a>
           </li>
