7f61c769044c02bf665dff655c26a146f837afea
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-09-11 19:02:26 -0500

    Added search form components

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
 
-function Search() {
-    return (
-        <Container fluid>
-            <Row>
-                <Col size="md-12">
-                    <Jumbotron>
-                        <h1>Search</h1>
-                    </Jumbotron>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+class Search extends Component {
+    state = {
+        search: "",
+        breeds: [],
+        results: [],
+        error: ""
+    };
+
+    handleInputChange = event => {
+        this.setState({ search: event.target.value });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log("Clickhy HI");
+    };
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            <h1>React Google Books Search</h1>
+                            <h2>Search for and Save Books of Interest</h2>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <SearchForm
+                            handleFormSubmit={this.handleFormSubmit}
+                            handleInputChange={this.handleInputChange}
+                            breeds={this.state.breeds}
+                        />
+                        <SearchResults results={this.state.results} />
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+};
 
 export default Search;
