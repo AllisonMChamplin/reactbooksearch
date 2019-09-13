21b413a52aba197e673f312bdde337833e064fc1
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-09-13 11:31:40 -0500

    Removed some boilerplate bootstrap grid components. CSS Updates

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import { Col, Row, Container } from "../components/Grid";
+// import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
@@ -38,26 +38,24 @@ class Search extends Component {
 
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <Col size="md-12">
-                        <Jumbotron>
-                            <h1>React Google Books Search</h1>
-                            <h2>Search for and Save Books of Interest</h2>
-                        </Jumbotron>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size="md-12">
-                        <SearchForm
-                            handleInputChange={this.handleInputChange}
-                            books={this.state.books}
-                            handleFormSubmit={this.handleFormSubmit}
-                        />
-                        <SearchResults results={this.state.results} />
-                    </Col>
-                </Row>
-            </Container>
+            <>
+                <Jumbotron>
+                    <h1>React Google Books Search</h1>
+                    <h2>Search for and Save Books of Interest</h2>
+                </Jumbotron>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-8 mx-auto">
+                            <SearchForm
+                                handleInputChange={this.handleInputChange}
+                                books={this.state.books}
+                                handleFormSubmit={this.handleFormSubmit}
+                            />
+                            <SearchResults results={this.state.results} />
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 };
